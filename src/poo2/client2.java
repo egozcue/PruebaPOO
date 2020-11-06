d8a3b0f4942ac6ef94ab921782efe5a6ede4c2bc
egozcue <72351865+egozcue@users.noreply.github.com>
2020-11-06 17:12:51 +0100

    Update client2.java

--- src/poo2/client2.java
+++ src/poo2/client2.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package poo2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -15,39 +11,45 @@ import java.util.Scanner;
  * @author egozc
  */
 public class client2 {
+
     private final String address;
-    private static Map<Integer, checking2> chAcc;
+    private static HashMap<String, checking2> chAcc;
     private final String name;
-    private final int pin;
+    private int pin;
     private final int ssn;
     private boolean status;
     private final int phone;
-
-
-    public client2(String address, String name, int pin, int ssn, boolean status, int phone) {
-        this.address = address;
-        this.chAcc=chAcc;
-        this.name = name;
-        this.pin = pin;
-        this.ssn = ssn;
-        this.status = status;
-        this.phone = phone;
+    Scanner SC = new Scanner(System.in);
+    public client2(int ssn) {
+        this.ssn=ssn;
+        System.out.println("Address:");
+        this.address = SC.nextLine();
+        System.out.println("Name:");
+        this.name =SC.nextLine();
+        this.status = true;
+        System.out.println("Telephone:");
+        this.phone = SC.nextInt();
     }
 
+    public String getName(){
+        return name;
+    }
+    public i
+    public void delete(int ssn) {
 
+    }
 
-    public void create(String address, String name, int pin, int ssn, boolean status, int phone){
+    public void hasCheckAcc(int pin) {
         
-}
+        
+    }
 
-public void delete(int ssn){
-    
-}   
-
-public void hasCheckAcc(int pin){
-    
-}
-public void show(int ssn){
-    
-}
+    public void show(int ssn) {
+        System.out.println("Account of" +getName());
+        System.out.println("---------------");
+        System.out.println("Checking account number"+);
+    }
+    public int getssn(){
+        
+    }
 }
