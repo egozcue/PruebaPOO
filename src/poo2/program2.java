8e34b1b42fd7e0b67d85f7f9a849780f00087cc5
alexgczs <62362774+alexgczs@users.noreply.github.com>
2020-11-06 19:02:39 +0100

    Update program2.java

--- src/poo2/program2.java
+++ src/poo2/program2.java
@@ -5,17 +5,75 @@
  */
 package poo2;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import static javax.swing.text.html.HTML.Tag.S;
 /**
  *
  * @author egozc
  */
 public class program2 {
-    public static void main(String[] args) {
-        
-    }
-    public static void checkingMenu
+    private static List<client2>clients;
+    private static int index;
     
+    public static void menu(){
+        System.out.println("[C]reate Customer [D]elete Customer Dis[p]lay [S]ervices [Q]uit");
+    }
+    
+    public static void election(){
+        String choose;
+        do{
+        menu();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Choose an option: ");
+        choose=sc.nextLine();
+        switch(choose){
+            case "C":
+                do{
+                    int ssn = sc.nextInt();
+                    confirmSsn(clients,ssn);
+                }
+                while(confirmSsn(clients,ssn)<)
+
+                new client2(ssn);
+                
+                break;
+            case "D":
+                delete();
+                break;
+            case "p":
+                display();
+                break;
+            case "s":
+                services();
+                index=confirmarSsn(clients,ssn);
+                }while (index>0);
+                break;
+            case "q":
+                break;
+        }
+        }
+    
+        while (!("q".equals(choose)));
+                    
+        
+                
+                
+        }
+    public static int confirmSsn(List<client2> lista,int ssn){
+        int x=-1;
+        for (int i=0; i<lista.size(); i++){
+            if(lista.get(i).getssn()==ssn){
+                x=i;
+            }
+        }
+        return x;
+    }
+        
+    
+    public static void main(String[] args) {
+        election();
+    }
+   
 }
