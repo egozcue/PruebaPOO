5b7d180538f1fff3e0862fe75ef7853facb8bccc
alexgczs <62362774+alexgczs@users.noreply.github.com>
2020-11-08 15:29:25 +0100

    Update checking2.java

--- src/poo2/checking2.java
+++ src/poo2/checking2.java
@@ -3,75 +3,126 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package poo2;
 
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
+import static poo2.client2.comprobarSsn;
+import static poo2.client2.map;
+import static poo2.client2.getStatus;
         
 /**
  *
- * @author egozc
+ * @author 
  */
 public class checking2 {
-    private double balance;
-    private final int number;
-    private boolean status;
-    Scanner SC = new Scanner(System.in);
-    
-    public checking2(){
-        Random rand = new Random();
-        number=(int)(Math.random() * (99999 - 10000 + 1) + 10000);
-    }
+    private static double balance;
+    private static int pin;
+    private static boolean status;
+    static Scanner SC = new Scanner(System.in);
+    static HashMap <String, Double> mapPin = new HashMap <String, Double> ();
 
     /**
      *
-     * @param resp
+     * @param balance
+     * @param number
+     * @param status
+     */
+    public checking2(double balance, int pin, boolean status) {
+        checking2.balance = balance;
+        checking2.pin = pin;
+        checking2.status = status;
+    }
+    
+  
+
+    /**
+     *Random rand = new Random();
+        number=(int)(Math.random() * (99999 - 10000 + 1) + 10000);
+    
      */
     
-    }
-    public void menu(String resp){
+    
+    public static void menuServ(){
+        String choose;
         do{
             System.out.println("[O]pen C[L]ose [I]nq [D]ep [W]ithdraw [R]eturn");
-            switch (resp){
+            choose=SC.nextLine();
+            int ssn;
+            switch (choose){
                 case "O":
-                    open();
+                    System.out.println("Your ssn: ");
+                    ssn=SC.nextInt();
+                    if (comprobarSsn(ssn)){
+                        open(ssn);
+                    }
+                    else{
+                        System.out.println("The ssn "+ssn+ " is not in our database\n");
+                        System.out.println("First create a customer with that ssn");
+                    }
+                    break;
                 case "L":
                     close();
+                     break;
                 case "I":
                     inquire();
+                     break;
                 case "D":
                     System.out.println("Deposit:");
                     deposit(SC.nextDouble());
+                     break;
                 case "W":
                     System.out.println("Introduce the amount of cash you want: ");
                     withdraw(SC.nextDouble());
+                     break;
+                case "R":
+                    break;
             }
-        }while("R".equals(resp));
+        }while("R".equals(choose));
     }
-    public void open(){
+
+    public double getBalance(){
+    return balance;
+}
+    public static void open(int ssn){
+        client2 data=(client2) map.get(String.valueOf(ssn));
+        status= data.getStatus();
         if (!status){
-            status=true;
+            System.out.println("Choose your pin: ");
+            int pin=SC.nextInt();
             System.out.println("Initial Deposit:");
             balance=SC.nextDouble();
-            System.out.println("The checking account has been opened for Jane Doe");
+            System.out.println("The checking account had been opened");
+            status=true;
+            mapPin.put(String.valueOf(pin), balance);
         }
         else{
-            System.out.println("The account is already opened");
+            System.out.println("An account is already opened");
         }   
     }
-    public void close(){
-        status=false;
-        System.out.println("The account has been closed");
+    public static void close(){
+        System.out.println("Your pin: ");
+        String pin=SC.nextLine();
+        if (mapPin.containsKey(pin)){
+            System.out.println("The account has been closed");
+            mapPin.remove(pin);
+        }
+        else{
+            System.out.println("There´s no account with that pin ih our database");
+        }
+        
     }
-    public void inquire(){
+    public static void inquire(){
         System.out.println("-------------------");
-        System.out.println("Checkimg Account Number: "+String.valueOf(number));
-        System.out.println("Current Balance: "+String.valueOf(balance)+" â‚¬");
+        System.out.println("Checking Account Number: "+String.valueOf(number));
+        System.out.println("Current Balance: "+String.valueOf(balance));
         System.out.println("-------------------");
     }
-    public void withdraw(double cash){
+    public static void withdraw(double cash){
             if (cash<balance){
                 balance=balance-cash;
             }
@@ -79,10 +130,12 @@ public class checking2 {
                 System.out.println("The cash exceeds the balance");
             }
     }
-    public void deposit(double cash){
-        balance=balance+cash
-        System.out.println("Balance: "+String.valueOf(balance)+" â‚¬");
+    public static void deposit(double cash){
+        balance=balance+cash;
+        System.out.println("Balance: "+String.valueOf(balance));
     }
+
+}
         
     
-}
+
